0976e161b538def9f3a792f763a8aec14f6e8c7c
Teebo <ngubanethabo.ambrose@gmail.com>
2017-05-18 19:18:11 +0200

    Project

---
+++ prop.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV_import_export
+{
+
+	// properties
+	class prop
+	{
+		//******************************************************
+		// conection string .
+		public static string sqlConnString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\tmakoba\Desktop\CSV_import_export\CSV_DB.mdf;Integrated Security=True";
+		//******************************************************
+
+
+	}
+}
